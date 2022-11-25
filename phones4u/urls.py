c80647d56d34f411d132061ca3eb3062a0bd085a
shamiltharayil <30173209+shamiltharayil@users.noreply.github.com>
2022-11-25 17:58:49 +0530

    hi

--- phones4u/urls.py
+++ phones4u/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
     path('orders/',include('orders.urls')),
     path('adminapp/',include('adminapp.urls'))
     ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
 
  
 
